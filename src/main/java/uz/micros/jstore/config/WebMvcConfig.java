c1dd5cfc193d3f922d99715e73f827409da24262
serebrus <lordhasan@mail.ru>
2014-05-19 20:05:12 +0500

    bootstrap added

--- src/main/java/uz/micros/jstore/config/WebMvcConfig.java
+++ src/main/java/uz/micros/jstore/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package uz.micros.jstore.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -13,4 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @ComponentScan
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/**");
+    }
 }
---
+++ src/main/webapp/resources/js/site.js
@@ -0,0 +1,6 @@
+/**
+ * Created by java on 19.05.14.
+ */
+function showWelcome(obj) {
+    alert(obj.value);
+}
